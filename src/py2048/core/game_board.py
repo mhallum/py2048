be43e0713e6f8803044b218a11e8478c2f74836f
mhallum <mkhallum92@gmail.com>
2025-07-22 22:16:00 -0700

    feat (core): :sparkles: implement and test ability to shift the board left - Add test for shifting the board left - Implement ability to shift the board left - Add missing docstring

--- src/py2048/core/game_board.py
+++ src/py2048/core/game_board.py
@@ -9,7 +9,7 @@ class GameBoard:
     """Class representing the game board for 2048."""
 
     def __init__(self):
-        self.grid = [[0] * 4 for _ in range(4)]
+        self.grid: list[list[int]] = [[0] * 4 for _ in range(4)]
         self._add_random_tile()
         self._add_random_tile()
 
@@ -19,3 +19,21 @@ class GameBoard:
         ]:
             i, j = random.choice(empty_tiles)
             self.grid[i][j] = 2
+
+    def shift_left(self):
+        """Shift tiles to the left."""
+        for row in self.grid:
+            new_row = [tile for tile in row if tile != 0]
+            merged_row: list[int] = []
+            skip = False
+            for i, tile in enumerate(new_row):
+                if skip:
+                    skip = False
+                    continue
+                if i < len(new_row) - 1 and tile == new_row[i + 1]:
+                    merged_row.append(new_row[i] * 2)
+                    skip = True
+                else:
+                    merged_row.append(new_row[i])
+            merged_row.extend([0] * (4 - len(merged_row)))
+            row[:] = merged_row
