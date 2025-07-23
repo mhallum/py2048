9afc3eeacf6bf899d5d2e0fd4c4c756502f7152a
mhallum <mkhallum92@gmail.com>
2025-07-22 22:53:03 -0700

    test(tests): :test_tube: add test for shifting the board right

--- tests/core/test_moves.py
+++ tests/core/test_moves.py
@@ -17,3 +17,19 @@ def test_shift_board_left_does_not_double_merge():
     board.grid = [[2, 2, 2, 2], [4, 4, 4, 0], [2, 0, 2, 0], [0, 0, 0, 0]]
     board.shift_left()
     assert board.grid == [[4, 4, 0, 0], [8, 4, 0, 0], [4, 0, 0, 0], [0, 0, 0, 0]]
+
+
+def test_shift_board_right():
+    """Test that the game board shifts tiles to the right correctly."""
+    board = GameBoard()
+    board.grid = [[0, 0, 2, 2], [0, 0, 4, 4], [0, 2, 0, 2], [0, 0, 0, 0]]
+    board.shift_right()
+    assert board.grid == [[0, 0, 0, 4], [0, 0, 0, 8], [0, 0, 0, 4], [0, 0, 0, 0]]
+
+
+def test_shift_board_right_does_not_double_merge():
+    """Test that the game board does not double merge tiles when shifting right."""
+    board = GameBoard()
+    board.grid = [[2, 2, 2, 2], [0, 4, 4, 4], [0, 2, 0, 2], [0, 0, 0, 0]]
+    board.shift_right()
+    assert board.grid == [[0, 0, 4, 4], [0, 0, 4, 8], [0, 0, 0, 4], [0, 0, 0, 0]]
