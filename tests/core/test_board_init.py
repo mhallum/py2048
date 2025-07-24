766ccef66431fb5fb60383db43d8cfd6d15b82ea
mhallum <mkhallum92@gmail.com>
2025-07-24 09:37:13 -0700

    test(tests): :white_check_mark: add test for board inequality

--- tests/core/test_board_init.py
+++ tests/core/test_board_init.py
@@ -21,8 +21,13 @@ def test_board_can_be_created_with_already_loaded_tiles():
 
 def test_board_equality():
     """Test that two game boards with the same grid are equal."""
-    grid1 = [[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]]
-    grid2 = [[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]]
-    board1 = GameBoard(grid=grid1)
-    board2 = GameBoard(grid=grid2)
+    board1 = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
+    board2 = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
     assert board1 == board2
+
+
+def test_board_inequality():
+    """Test that two game boards with different grids are not equal."""
+    board1 = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
+    board2 = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 1, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
+    assert board1 != board2
