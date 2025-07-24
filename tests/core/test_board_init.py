7bbf62d10afd568ae64881f552837daefca39fda
mhallum <mkhallum92@gmail.com>
2025-07-24 09:35:27 -0700

    refactor(tests): :recycle: simplify board inizialization tests

--- tests/core/test_board_init.py
+++ tests/core/test_board_init.py
@@ -14,11 +14,9 @@ def test_board_initializes_with_two_tiles():
 
 def test_board_can_be_created_with_already_loaded_tiles():
     """Test that the game board can be created with already loaded tiles."""
-    initial_grid = [[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]]
-    board = GameBoard(grid=initial_grid)
-    board.grid = initial_grid
+    board = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
     filled = sum(1 for row in board.grid for tile in row if tile != 0)
-    assert filled == len([tile for row in initial_grid for tile in row if tile != 0])
+    assert filled == len([tile for row in board.grid for tile in row if tile != 0])
 
 
 def test_board_equality():
