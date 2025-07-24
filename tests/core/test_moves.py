6d6e2c9d397e6dcf65711f321fb131c95ab96dbc
mhallum <mkhallum92@gmail.com>
2025-07-24 11:36:42 -0700

    test(tests): add test to confirm that the board can spawn a tile

--- tests/core/test_moves.py
+++ tests/core/test_moves.py
@@ -3,6 +3,14 @@
 from py2048.core.models import GameBoard
 
 
+def test_board_can_spawn_tile():
+    """Test that a new tile is spawned on the board."""
+    board = GameBoard(grid=((0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0)))
+    new_board = board.spawn_tile()
+    filled_after_spawn = sum(1 for row in new_board.grid for tile in row if tile != 0)
+    assert filled_after_spawn == 1
+
+
 def test_board_can_shift_left():
     """Test that the game board shifts tiles to the left correctly."""
     board = GameBoard(((2, 2, 2, 2), (4, 4, 4, 0), (2, 0, 2, 0), (0, 0, 0, 0)))
