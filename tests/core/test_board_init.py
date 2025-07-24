bac4a948ee81c8d0ecd96a7a6ce648cc1c8d2ac5
mhallum <mkhallum92@gmail.com>
2025-07-24 11:07:59 -0700

    test(tests): :recycle: change lists to tuples

--- tests/core/test_board_init.py
+++ tests/core/test_board_init.py
@@ -1,14 +1,14 @@
 """Unit tests for GameBoard initialization"""
 
-from py2048.core import models
 from py2048.core.models import GameBoard
+from py2048.core import models
 
 EXPECTED_TILES = 2
 
 
 def test_spawn_tile_on_empty_board():
     """Test that a new tile is spawned on the board on an empty board."""
-    board = GameBoard(grid=[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    board = GameBoard(grid=((0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0)))
     new_board = models.spawn_tile(board)
     filled_after_spawn = sum(1 for row in new_board.grid for tile in row if tile != 0)
     assert filled_after_spawn == 1
@@ -16,7 +16,7 @@ def test_spawn_tile_on_empty_board():
 
 def new_test_spawn_tile_on_non_empty_board():
     """Test that a new tile is spawned on the board on a non-empty board."""
-    board = GameBoard(grid=[[2, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    board = GameBoard(grid=((2, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0)))
     new_board = models.spawn_tile(board)
     filled_after_spawn = sum(1 for row in new_board.grid for tile in row if tile != 0)
     expected_n_tiles = 2
@@ -32,20 +32,20 @@ def test_board_initializes_with_empty_grid_by_default():
 
 def test_board_can_be_created_with_already_loaded_tiles():
     """Test that the game board can be created with already loaded tiles."""
-    board = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
+    board = GameBoard(grid=((2, 0, 2, 0), (0, 0, 0, 0), (0, 2, 0, 2), (0, 0, 0, 0)))
     filled = sum(1 for row in board.grid for tile in row if tile != 0)
     assert filled == len([tile for row in board.grid for tile in row if tile != 0])
 
 
 def test_board_equality():
     """Test that two game boards with the same grid are equal."""
-    board1 = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
-    board2 = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
+    board1 = GameBoard(grid=((2, 0, 2, 0), (0, 0, 0, 0), (0, 2, 0, 2), (0, 0, 0, 0)))
+    board2 = GameBoard(grid=((2, 0, 2, 0), (0, 0, 0, 0), (0, 2, 0, 2), (0, 0, 0, 0)))
     assert board1 == board2
 
 
 def test_board_inequality():
     """Test that two game boards with different grids are not equal."""
-    board1 = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 0, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
-    board2 = GameBoard(grid=[[2, 0, 2, 0], [0, 0, 1, 0], [0, 2, 0, 2], [0, 0, 0, 0]])
+    board1 = GameBoard(grid=((2, 0, 2, 0), (0, 0, 0, 0), (0, 2, 0, 2), (0, 0, 0, 0)))
+    board2 = GameBoard(grid=((2, 0, 2, 0), (0, 0, 1, 0), (0, 2, 0, 2), (0, 0, 0, 0)))
     assert board1 != board2
--- tests/core/test_moves.py
+++ tests/core/test_moves.py
@@ -5,43 +5,43 @@ from py2048.core.models import GameBoard
 
 def test_shift_board_left():
     """Test that the game board shifts tiles to the left correctly."""
-    board = GameBoard([[2, 2, 0, 0], [4, 4, 0, 0], [2, 0, 2, 0], [0, 0, 0, 0]])
+    board = GameBoard(((2, 2, 0, 0), (4, 4, 0, 0), (2, 0, 2, 0), (0, 0, 0, 0)))
     board.shift_left()
     assert board == GameBoard(
-        ([[4, 0, 0, 0], [8, 0, 0, 0], [4, 0, 0, 0], [0, 0, 0, 0]])
+        (((4, 0, 0, 0), (8, 0, 0, 0), (4, 0, 0, 0), (0, 0, 0, 0)))
     )
 
 
 def test_shift_board_left_does_not_double_merge():
     """Test that the game board does not double merge tiles."""
-    board = GameBoard([[2, 2, 2, 2], [4, 4, 4, 0], [2, 0, 2, 0], [0, 0, 0, 0]])
+    board = GameBoard(((2, 2, 2, 2), (4, 4, 4, 0), (2, 0, 2, 0), (0, 0, 0, 0)))
     board.shift_left()
-    assert board == GameBoard([[4, 4, 0, 0], [8, 4, 0, 0], [4, 0, 0, 0], [0, 0, 0, 0]])
+    assert board == GameBoard(((4, 4, 0, 0), (8, 4, 0, 0), (4, 0, 0, 0), (0, 0, 0, 0)))
 
 
 def test_shift_board_right():
     """Test that the game board shifts tiles to the right correctly."""
-    board = GameBoard([[0, 0, 2, 2], [0, 0, 4, 4], [0, 2, 0, 2], [0, 0, 0, 0]])
+    board = GameBoard(((0, 0, 2, 2), (0, 0, 4, 4), (0, 2, 0, 2), (0, 0, 0, 0)))
     board.shift_right()
-    assert board == GameBoard([[0, 0, 0, 4], [0, 0, 0, 8], [0, 0, 0, 4], [0, 0, 0, 0]])
+    assert board == GameBoard(((0, 0, 0, 4), (0, 0, 0, 8), (0, 0, 0, 4), (0, 0, 0, 0)))
 
 
 def test_shift_board_right_does_not_double_merge():
     """Test that the game board does not double merge tiles when shifting right."""
-    board = GameBoard([[2, 2, 2, 2], [0, 4, 4, 4], [0, 2, 0, 2], [0, 0, 0, 0]])
+    board = GameBoard(((2, 2, 2, 2), (0, 4, 4, 4), (0, 2, 0, 2), (0, 0, 0, 0)))
     board.shift_right()
-    assert board == GameBoard([[0, 0, 4, 4], [0, 0, 4, 8], [0, 0, 0, 4], [0, 0, 0, 0]])
+    assert board == GameBoard(((0, 0, 4, 4), (0, 0, 4, 8), (0, 0, 0, 4), (0, 0, 0, 0)))
 
 
 def test_shift_board_up():
     """Test that the game board shifts tiles up correctly."""
-    board = GameBoard([[2, 2, 2, 2], [2, 2, 0, 2], [0, 2, 2, 4], [0, 2, 0, 0]])
+    board = GameBoard(((2, 2, 2, 2), (2, 2, 0, 2), (0, 2, 2, 4), (0, 2, 0, 0)))
     board.shift_up()
-    assert board == GameBoard([[4, 4, 4, 4], [0, 4, 0, 4], [0, 0, 0, 0], [0, 0, 0, 0]])
+    assert board == GameBoard(((4, 4, 4, 4), (0, 4, 0, 4), (0, 0, 0, 0), (0, 0, 0, 0)))
 
 
 def test_shift_board_down():
     """Test that the game board shifts tiles down correctly."""
-    board = GameBoard([[0, 2, 0, 0], [2, 2, 2, 2], [2, 2, 0, 2], [0, 2, 2, 4]])
+    board = GameBoard(((0, 2, 0, 0), (2, 2, 2, 2), (2, 2, 0, 2), (0, 2, 2, 4)))
     board.shift_down()
-    assert board == GameBoard([[0, 0, 0, 0], [0, 0, 0, 0], [0, 4, 0, 4], [4, 4, 4, 4]])
+    assert board == GameBoard(((0, 0, 0, 0), (0, 0, 0, 0), (0, 4, 0, 4), (4, 4, 4, 4)))
