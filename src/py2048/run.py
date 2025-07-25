fb179897fb7930ccb2be8696a0c188bde683ea08
mhallum <mkhallum92@gmail.com>
2025-07-25 14:57:38 -0700

    feat(ui): :sparkles: implement --mode option for unified entry point

--- src/py2048/run.py
+++ src/py2048/run.py
@@ -1,7 +1,19 @@
 """Unified entry point for Py2048.
 This script allows launching the game in CLI, GUI, or Web mode."""
 
+from typing import Literal
+from enum import Enum
+
 import click
+
+
+class Mode(str, Enum):
+    """Enumeration for game modes."""
+
+    CLI = "cli"
+    GUI = "gui"
+    WEB = "web"
+
 
 CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 
@@ -10,8 +22,27 @@ CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 @click.version_option(
     None, "-v", "--version", prog_name="Py2048", message="%(prog)s version %(version)s"
 )
-def main():
-    """Launch Py2048 in CLI, GIU, or Web mode."""
+@click.option(
+    "-m",
+    "--mode",
+    type=click.Choice([m.value for m in Mode], case_sensitive=False),
+    default=Mode.CLI.value,
+    help="Select the mode to run the game: CLI, GUI, or Web.",
+)
+def main(mode: Literal["cli", "gui", "web"] = "cli") -> None:
+    """Launch Py2048 in CLI, GUI, or Web mode."""
+    match Mode(mode.lower()):
+        case Mode.CLI:
+            click.echo("Launching Py2048 in CLI mode")
+            # call_cli()
+        case Mode.GUI:
+            click.echo("Launching Py2048 in GUI mode (Coming soon)")
+            # call_gui()
+        case Mode.WEB:
+            click.echo("Launching Py2048 in Web mode (Coming soon)")
+            # call_web()
+        case _:
+            click.echo("Invalid mode selected.")
 
 
 if __name__ == "__main__":
