f50c9e7825de87a0cac627e46d34cb89c9f1d6ca
mhallum <mkhallum92@gmail.com>
2025-07-24 22:52:21 -0700

    feat(cli): :sparkles: implement -h option

--- src/py2048/front_end/cli.py
+++ src/py2048/front_end/cli.py
@@ -1,10 +1,14 @@
+"""CLI for the Py2048 game."""
+
 import click
 
+CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 
-@click.command()
+
+@click.command(context_settings=CONTEXT_SETTINGS)
 def py2048():
     """Py2048 is a Python implementation of the popular 2048 sliding puzzle game."""
 
 
 if __name__ == "__main__":
-    py2048()
+    py2048()  # pragma: no cover
