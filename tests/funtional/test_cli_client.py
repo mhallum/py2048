62b5b85df720fd64ccc0ac32ca15e70bded66a0b
mhallum <mkhallum92@gmail.com>
2025-07-24 22:59:54 -0700

    test(tests): :test_tube: add test ensuring that py2048 has a  --version options

--- tests/funtional/test_cli_client.py
+++ tests/funtional/test_cli_client.py
@@ -7,16 +7,24 @@ from py2048.front_end.cli import py2048
 class TestCli:
     """Test cases for the CLI client of the py2048 game."""
 
-    def test_py2048_has_help_command(self):
+    def test_py2048_has_help_option(self):
         """Test that typing 'py2048 --help' shows the help message."""
         runner = CliRunner()
         result = runner.invoke(py2048, ["--help"])
         assert result.exit_code == 0
         assert "Usage: py2048" in result.output
 
-    def test_py2048_has_short_help_command(self):
+    def test_py2048_has_short_help_option(self):
         """Test that typing 'py2048 -h' shows the help message."""
         runner = CliRunner()
         result = runner.invoke(py2048, ["-h"])
         assert result.exit_code == 0
         assert "Usage: py2048" in result.output
+
+    def test_py2048_has_version_option(self):
+        """Test that typing 'py2048 --version' shows the version."""
+        runner = CliRunner()
+        result = runner.invoke(py2048, ["--version"])
+        assert result.exit_code == 0
+        assert "py2048" in result.output
+        assert "version" in result.output
