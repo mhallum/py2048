86ec0ee20473699384992efe5613d8d323e151d2
mhallum <mkhallum92@gmail.com>
2025-07-25 14:30:40 -0700

    test(tests): :test_tube: add test for unified entry point mode selection default

--- tests/functional/test_cli_client.py
+++ tests/functional/test_cli_client.py
@@ -3,38 +3,35 @@
 # pylint: disable=magic-value-comparison, no-self-use, too-few-public-methods
 
 from click.testing import CliRunner
+import pytest
 from py2048.run import main
 
 
-class TestCli:
-    """Test cases for the CLI client of the py2048 game."""
+class TestUnifiedEntryPoint:
+    """Test cases for the unified entry point of the Py2048 game."""
 
-    def test_py2048_has_help_option(self):
-        """Test that invoking the '--help' option shows the help message."""
+    @pytest.mark.parametrize("flag", ["--help", "-h"])
+    def test_help_option(self, flag: str):
+        """Test that invoking the '-h or --help' option shows the help message."""
         runner = CliRunner()
-        result = runner.invoke(main, ["--help"])
+        result = runner.invoke(main, [flag])
         assert result.exit_code == 0
         assert "Usage: " in result.output
 
-    def test_py2048_has_short_help_option(self):
-        """Test that invoking the '-h' option shows the help message."""
+    @pytest.mark.parametrize("flag", ["--version", "-v"])
+    def test_version_option(self, flag: str):
+        """Test that invoking the '-v or --version' option shows the version."""
         runner = CliRunner()
-        result = runner.invoke(main, ["-h"])
-        assert result.exit_code == 0
-        assert "Usage: " in result.output
-
-    def test_py2048_has_version_option(self):
-        """Test that invoking the '--version' option shows the version."""
-        runner = CliRunner()
-        result = runner.invoke(main, ["--version"])
+        result = runner.invoke(main, [flag])
         assert result.exit_code == 0
         assert "Py2048" in result.output
         assert "version" in result.output
 
-    def test_py2048_has_short_version_option(self):
-        """Test that invoking the '-v' option shows the version."""
+    def test_mode_selection_default(self):
+        """Test that the default mode is CLI when no mode is specified."""
         runner = CliRunner()
-        result = runner.invoke(main, ["-v"])
+        result = runner.invoke(main)
         assert result.exit_code == 0
-        assert "Py2048" in result.output
-        assert "version" in result.output
+        assert (
+            "Launching Py2048 in CLI mode" in result.output
+        )  # Placeholder for actual CLI mode message
