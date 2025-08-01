4d5cbfb7b1e38d734d1294f21bab56f91d24d694
mhallum <mkhallum92@gmail.com>
2025-08-01 02:43:48 -0700

    feat(tests): add test fixture for a test game

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+from py2048.core import models
+
+
+@pytest.fixture
+def test_game() -> models.Py2048Game:
+    """Fixture to create a test game instance."""
+    board = models.GameBoard(
+        grid=((0, 0, 2, 0), (0, 0, 0, 0), (0, 0, 2, 0), (0, 0, 0, 0))
+    )
+    state = models.GameState(board=board, score=0)
+    return models.Py2048Game(game_id="test_game", state=state)
